b4ef9d8c2409f87db3080a1bcf38f923a1ea27b4
Ivan753 <30318463+Ivan753@users.noreply.github.com>
2017-10-20 18:43:31 +0300

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 
-main(){
+int main(){
 
-char s[20];
-int i, number;
-char t;
+char s;
+int number;
 double res[2];
 
-scanf("%d%s",&number,s);
 
-for(i=0; i<20; i++){
-if(s[i]=='C' || s[i]=='F' || s[i]=='K'){
-t = s[i];
-break;
-}
-}
+scanf("%d%c",&number,&s);
 
-switch(t){
+switch(s){
 
 case 'C':  
 	if (number>=-273){
@@ -29,7 +22,7 @@ case 'C':
 break;
 
 case 'F':  
-	if (number>=0){
+	if (number>=-459){
 	res[0] = (number - 32.0)*5.0/9.0; //For C
 	res[1] = res[0] + 273.15; //For K
 	printf("%.2f C %.2f K",res[0],res[1]);
@@ -46,7 +39,36 @@ case 'K':
 	printf("The entered temperature is below absolute zero");
 	}
 break;
-default: printf("Enter the unit of measure"); break;
+default:
+
+printf("%d C:\n", number);
+if (number>=-273){
+	
+	res[0] = number + 273.15; //For K
+	res[1] = number*9.0/5.0 + 32.0; //For F
+	printf("%.2f K %.2f F\n\n",res[0],res[1]);
+	}else{
+	printf("The entered temperature is below absolute zero (C)\n");
+	}
+	
+	printf("%d F:\n", number);
+	if (number>=-459){	
+	res[0] = (number - 32.0)*5.0/9.0; //For C
+	res[1] = res[0] + 273.15; //For K
+	printf("%.2f C %.2f K\n\n",res[0],res[1]);
+	}else{
+	printf("The entered temperature is below absolute zero (F)\n");
+	}
+	printf("%d K:\n", number);
+	if (number>=0){
+	res[0] = number - 273.15; //For C
+	res[1] = res[0]*9.0/5.0 + 32.0; //For F
+	printf("%.2f C %.2f F\n",res[0],res[1]);
+	}else{
+	printf("The entered temperature is below absolute zero (K)\n");
+	}
+break;
 }
 
+return 0;
 }
