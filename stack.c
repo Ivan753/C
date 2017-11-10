fbc07722197ee9c3a2e9b5071e14eb3861390c92
Ivan753 <30318463+Ivan753@users.noreply.github.com>
2017-11-10 22:22:56 +0300

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void push(int *x, int a, short *n){
+
+	x[*n] = a;
+	(*n)++;
+
+}
+
+int pull(int *x, short *n){
+
+	int res = x[*n - 1];
+	x[*n - 1] = 0;
+	(*n)--;
+	return res;
+
+}
+
+
+int main(){
+	
+int stack[20];
+short *count;
+int i,b;	
+
+
+*count = 0;
+
+for(i = 0; i < 20; i++) stack[i] = 0;
+
+push(stack, 34, count);
+push(stack, -56, count);
+push(stack, 34, count);
+push(stack, 100034, count);
+
+for(i = 0; i < 20; i++){
+	printf("%d ", stack[i]);
+}	
+
+printf("\n");
+
+b = pull(stack, count);
+
+for(i = 0; i < 20; i++){
+	printf("%d ", stack[i]);
+}	
+
+printf("\n%d ", b);
+
+}
