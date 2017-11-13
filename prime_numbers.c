9acedad2cbf9740320fc7efce2724f7ddfda7929
Ivan753 <30318463+Ivan753@users.noreply.github.com>
2017-11-13 11:15:22 +0300

    Add files via upload

---
+++ prime_numbers.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int c = 72;
+    int i, t;
+    t = 1;
+    int b,e;
+
+    b = 0;
+    e = 0;
+
+    if(c%2 !=0){
+
+        b = 3;
+        e = 2;
+
+
+        for(i = b; i <= c/2; i+= e){
+
+            if(c%i == 0){
+                t = 0;
+                break;
+            }
+
+        }
+
+    }else{
+        t = 0;
+    }
+
+    if(t){
+        printf("True\n");
+    }else{
+        printf("False\n");
+    }
+
+    return 0;
+}
