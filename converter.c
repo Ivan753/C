ceb5b5f8fa11af0097f6fdfedce597a0f2a7cb9d
Ivan753 <30318463+Ivan753@users.noreply.github.com>
2017-11-23 22:54:11 +0300

    Update converter.c

--- converter.c
+++ converter.c
@@ -18,8 +18,8 @@ switch(s){
 
 case 'C':  
 	if (number>=-273){
-	res[0] = number + 273.15; //For K
-	res[1] = number*9.0/5.0 + 32.0; //For F
+	res[0] = number + 273.15; /*For K*/
+	res[1] = number*9.0/5.0 + 32.0; /*For F*/
 	printf("%.2f K %.2f F",res[0],res[1]);
 	}else{
 	printf("The entered temperature is below absolute zero");
@@ -28,8 +28,8 @@ break;
 
 case 'F':  
 	if (number>=-459){
-	res[0] = (number - 32.0)*5.0/9.0; //For C
-	res[1] = res[0] + 273.15; //For K
+	res[0] = (number - 32.0)*5.0/9.0; /*For C*/
+	res[1] = res[0] + 273.15; /*For K*/
 	printf("%.2f C %.2f K",res[0],res[1]);
 	}else{
 	printf("The entered temperature is below absolute zero");
@@ -37,8 +37,8 @@ case 'F':
 break;
 case 'K':
 	if (number>=0){
-	res[0] = number - 273.15; //For C
-	res[1] = res[0]*9.0/5.0 + 32.0; //For F
+	res[0] = number - 273.15; /*For C*/
+	res[1] = res[0]*9.0/5.0 + 32.0; /*For F*/
 	printf("%.2f C %.2f F",res[0],res[1]);
 	}else{
 	printf("The entered temperature is below absolute zero");
@@ -49,8 +49,8 @@ default:
 printf("%d C:\n", number);
 if (number>=-273){
 	
-	res[0] = number + 273.15; //For K
-	res[1] = number*9.0/5.0 + 32.0; //For F
+	res[0] = number + 273.15; /*For K*/
+	res[1] = number*9.0/5.0 + 32.0; /*For F*/
 	printf("%.2f K %.2f F\n\n",res[0],res[1]);
 	}else{
 	printf("The entered temperature is below absolute zero (C)\n");
@@ -58,16 +58,16 @@ if (number>=-273){
 	
 	printf("%d F:\n", number);
 	if (number>=-459){	
-	res[0] = (number - 32.0)*5.0/9.0; //For C
-	res[1] = res[0] + 273.15; //For K
+	res[0] = (number - 32.0)*5.0/9.0; /*For C*/
+	res[1] = res[0] + 273.15; /*For K*/
 	printf("%.2f C %.2f K\n\n",res[0],res[1]);
 	}else{
 	printf("The entered temperature is below absolute zero (F)\n");
 	}
 	printf("%d K:\n", number);
 	if (number>=0){
-	res[0] = number - 273.15; //For C
-	res[1] = res[0]*9.0/5.0 + 32.0; //For F
+	res[0] = number - 273.15; /*For C*/
+	res[1] = res[0]*9.0/5.0 + 32.0; /*For F*/
 	printf("%.2f C %.2f F\n",res[0],res[1]);
 	}else{
 	printf("The entered temperature is below absolute zero (K)\n");
